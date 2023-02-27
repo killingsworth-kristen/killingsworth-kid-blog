cd4044bfa04a4d9bbe18ce69e57893f1e4995eb5
kristen-killingsworth <killingsworth_kristen@yahoo.com>
2023-02-27 14:41:41 -0500

    style tweaks for footer/poll

--- src/components/pages/Poll.js
+++ src/components/pages/Poll.js
@@ -5,10 +5,12 @@ export default function Poll () {
     return (
         <>
         <section className='poll-container'>
-        <div className='iframe-container'>
-            <iframe className="iframe" src="https://docs.google.com/forms/d/e/1FAIpQLSfRy9FkVlq5VdMT7aKs73MvrBxOdSNY1Y3jsDCbqTgDqNzKKA/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-        </div>
-
+            <div className='poll-title-container'>
+                <h3 className='poll-title'>Win a prize if you have the most correct/closest guesses about the baby!</h3>
+            </div>
+            <div className='iframe-container'>
+                <iframe className="iframe" src="https://docs.google.com/forms/d/e/1FAIpQLSfRy9FkVlq5VdMT7aKs73MvrBxOdSNY1Y3jsDCbqTgDqNzKKA/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            </div>
         </section>
         </>
     )
