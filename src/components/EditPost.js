71e5c949227cea0571f738c124753189d89033c2
kristen-killingsworth <killingsworth_kristen@yahoo.com>
2023-03-14 19:42:04 -0400

    troubleshooting deploy; null check on editPost

--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -15,7 +15,7 @@ export default function EditPost ({postMode, setPostMode, user}) {
 
     useEffect(()=>{
         const editTarget = localStorage.getItem('edit')
-        if (editTarget === '') {
+        if (editTarget === '' || null) {
             return;
         } else {
             const blogCardNum = editTarget.split(' ')[1]
