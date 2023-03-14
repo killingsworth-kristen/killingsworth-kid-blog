6b749ffbbf5d45ec6890fedc3988c35fd55dff96
kristen-killingsworth <killingsworth_kristen@yahoo.com>
2023-03-14 19:33:22 -0400

    troubleshooting deploy

--- src/components/CloudinaryWidget.js
+++ src/components/CloudinaryWidget.js
@@ -7,8 +7,8 @@ export default function CloudinaryWidget ({setPostImg, postMode}) {
     const handleWidgetOpen = () => {
         const widget = window.cloudinary.createUploadWidget(
             {
-                cloudName: process.env.REACT_APP_CLOUD_NAME,
-                uploadPreset: process.env.REACT_APP_UPLOAD_PRESET,
+                cloudName: "dwe2wo8w5",
+                uploadPreset: "v0udxsbg",
             },
             (error, result) => {
                 if (result.event === "success") {
